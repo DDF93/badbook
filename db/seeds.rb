665900865bfe1e25f7f1e69d2078b1f785dd2054
Eleanor Vazraghi <eleanorvazraghi@gmail.com>
2024-03-09 13:10:11 +0000

    carousel and tile work so far

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
     genre: Faker::Book.genre,
     date: Faker::Date.between(from: 100.years.ago, to: Date.today),
     description: Faker::Lorem.paragraph,
-    image_url: "https://media.gettyimages.com/id/157482029/photo/stack-of-books.jpg?s=612x612&w=gi&k=20&c=_Yaofm8sZLZkKs1eMkv-zhk8K4k5u0g0fJuQrReWfdQ="
+    image_url: "https://media.gettyimages.com/id/157482029/photo/stack-of-books.jpg"
   )
 end
 
