f52623cadd549e7cada56623b08885427a8cba8a
Dominic Welds <dominicwelds@googlemail.com>
2024-03-09 15:22:30 +0000

    Changed seed file

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,14 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @initial_books = ["I12oPwAACAAJ", "u7XrDwAAQBAJ", "xDtkEAAAQBAJ",]
+  end
+
+  private
+
+  def book_by_id
+    data = URI.open('https://www.googleapis.com/books/v1/#{book_id}').read
+    book_json = JSON.parse(data)
+    Book.create(title: book_json['title'], author: book_json['author'])
   end
 end
