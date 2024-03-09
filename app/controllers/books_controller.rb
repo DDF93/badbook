9834bbf93d18d7e1416cf5f77d8d0d1250cd905b
Dominic Welds <dominicwelds@googlemail.com>
2024-03-09 17:43:58 +0000

    Do not use test index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    # @horror_books = get_books_by_genre('Horror')
     @bookclub_collection = Book.all.sample(15)
     read_books_shelf = current_user.bookshelves.find_by(name: 'Read Books')
     @read_books = read_books_shelf.present? ? read_books_shelf.books.to_a : []
@@ -24,4 +25,29 @@ private
   def set_book
     @book = Book.find(params[:id])
   end
-end
+
+#   def get_books_by_genre(genre)
+#     book_ids = []
+#     url = "https://www.googleapis.com/books/v1/volumes?q=subject:#{genre}&maxResults=20"
+#     data = URI.open(url).read
+#     book_json = JSON.parse(data)
+#     book_json["items"].each do |book|
+#     book_ids << book["id"]
+#     end
+#     books = []
+#     book_ids.each do |book_id|
+#     book_instance = Book.new(
+#       title: book_json.dig("volumeInfo", "title"),
+#       author: book_json.dig("volumeInfo", "authors")&.join(", "),
+#       date: book_json.dig("volumeInfo", "publishedDate"),
+#       description: book_json.dig("volumeInfo", "description"),
+#       p image_url: book_json.dig("volumeInfo", "imageLinks", "thumbnail"),
+
+#       google_id: book_id
+#     )
+
+#     books << book_instance
+#     end
+#     return books
+#   end
+# end
