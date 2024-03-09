3caa9bad1d50ea177a69d88174afc610db1e35f6
Dominic Welds <dominicwelds@googlemail.com>
2024-03-09 22:45:59 +0000

    Added genre search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,11 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    # @horror_books = get_books_by_genre('Horror')
+    @romance_books = get_books_by_genre('Romance')
+    @crime_books = get_books_by_genre('Crime')
+    @vampire_books = get_books_by_genre('Vampire')
+    @humor_books = get_books_by_genre('Humor')
+    @horror_books = get_books_by_genre('Horror')
     @bookclub_collection = Book.all.sample(15)
     read_books_shelf = current_user.bookshelves.find_by(name: 'Read Books')
     @read_books = read_books_shelf.present? ? read_books_shelf.books.to_a : []
@@ -26,28 +30,40 @@ private
     @book = Book.find(params[:id])
   end
 
-#   def get_books_by_genre(genre)
-#     book_ids = []
-#     url = "https://www.googleapis.com/books/v1/volumes?q=subject:#{genre}&maxResults=20"
-#     data = URI.open(url).read
-#     book_json = JSON.parse(data)
-#     book_json["items"].each do |book|
-#     book_ids << book["id"]
-#     end
-#     books = []
-#     book_ids.each do |book_id|
-#     book_instance = Book.new(
-#       title: book_json.dig("volumeInfo", "title"),
-#       author: book_json.dig("volumeInfo", "authors")&.join(", "),
-#       date: book_json.dig("volumeInfo", "publishedDate"),
-#       description: book_json.dig("volumeInfo", "description"),
-#       p image_url: book_json.dig("volumeInfo", "imageLinks", "thumbnail"),
+def get_books_by_genre(genre)
+  book_ids = []
+  url = "https://www.googleapis.com/books/v1/volumes?q=subject:#{genre}&maxResults=20"
+  data = URI.open(url).read
+  book_json = JSON.parse(data)
 
-#       google_id: book_id
-#     )
+  book_json["items"].each do |book|
+    book_ids << book["id"]
+  end
 
-#     books << book_instance
-#     end
-#     return books
-#   end
-# end
+  books = []
+
+  book_ids.each do |book_id|
+    id_data = URI.open("https://www.googleapis.com/books/v1/volumes/#{book_id}").read
+    id_json = JSON.parse(id_data)
+
+    title = id_json.dig("volumeInfo", "title")
+    authors = id_json.dig("volumeInfo", "authors")&.join(", ")
+    published_date = id_json.dig("volumeInfo", "publishedDate")
+    description = id_json.dig("volumeInfo", "description")
+    image_url = id_json.dig("volumeInfo", "imageLinks", "thumbnail")
+
+    book_add = {
+      google_id: book_id,
+      title: title,
+      author: authors,
+      date: published_date,
+      description: description,
+      image_url: image_url
+    }
+
+    books << book_add
+  end
+
+  books
+  end
+end
