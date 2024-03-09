63113320fbfc1bb0b15063021a39f74425df6d78
Dominic Welds <dominicwelds@googlemail.com>
2024-03-09 11:15:30 +0000

    Heroku push

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   validates :title, presence: true
   validates :author, presence: true
-  validates :year, presence: true
+  validates :date, presence: true
   validates :genre, presence: true
   validates :description, presence: true
 end
