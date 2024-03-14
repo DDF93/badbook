049d411750798712f826d0defd7aca55a1fcab40
Dominic Welds <dominicwelds@googlemail.com>
2024-03-14 18:54:40 +0000

    fdiaufbani

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
   resources :reviews, only: [:edit, :update, :show, :destroy]
-    resources :sessions, only: [:show] do
+  resources :sessions, only: [:show] do
       resources :attendees, only: [:create]
       post 'join', on: :member
-    end
+
   end
   resources :topics
   resources :bookshelf_books
