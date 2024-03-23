0c1529b0fac57118c533f54c16328d3f0604111c
Dominic Welds <dominicwelds@googlemail.com>
2024-03-23 09:45:17 +0000

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,101 +1,109 @@
 require 'json'
 require 'open-uri'
 
-initial_books = ["I12oPwAACAAJ", "u7XrDwAAQBAJ", "xDtkEAAAQBAJ", "49nUEAAAQBAJ", "hIx4EAAAQBAJ", "tkZFEAAAQBAJ", "Es_PCwAAQBAJ",
-                 "hxL2qWMAgv8C", "jVB1DwAAQBAJ", "nNjTDwAAQBAJ", "wxBbEAAAQBAJ", "cpAREAAAQBAJ", "OyCtDwAAQBAJ", "iBg_EAAAQBAJ", "lGjFtMRqp_YC"]
+bookclub_collection = ["I12oPwAACAAJ", "u7XrDwAAQBAJ", "xDtkEAAAQBAJ", "49nUEAAAQBAJ", "hIx4EAAAQBAJ", "tkZFEAAAQBAJ", "Es_PCwAAQBAJ",
+                       "hxL2qWMAgv8C", "jVB1DwAAQBAJ", "nNjTDwAAQBAJ", "wxBbEAAAQBAJ", "cpAREAAAQBAJ", "OyCtDwAAQBAJ", "iBg_EAAAQBAJ", "lGjFtMRqp_YC"]
 
-initial_books.each do |book_id|
-  begin
-    data = URI.open("https://www.googleapis.com/books/v1/volumes/#{book_id}").read
-    book_json = JSON.parse(data)
+def books_by_id(bookshelf_name, initial_books)
 
-    title = book_json.dig("volumeInfo", "title")
-    authors = book_json.dig("volumeInfo", "authors")&.join(", ")
-    published_date = book_json.dig("volumeInfo", "publishedDate")
-    description = book_json.dig("volumeInfo", "description")
+  new_bookshelf = Bookshelf.new(name: bookshelf_name)
 
-    Book.create(
-      google_id: book_id,
-      title: title,
-      author: authors,
-      date: published_date,
-      description: description,
-      image_url: book_json.dig("volumeInfo", "imageLinks", "thumbnail")
-    )
-  rescue StandardError => e
-    puts "Error occurred while processing book with ID #{book_id}: #{e.message}"
-  end
+  initial_books.each do |book_id|
+
+      data = URI.open("https://www.googleapis.com/books/v1/volumes/#{book_id}").read
+      book_json = JSON.parse(data)
+
+      title = book_json.dig("volumeInfo", "title")
+      authors = book_json.dig("volumeInfo", "authors")&.join(", ")
+      published_date = book_json.dig("volumeInfo", "publishedDate")
+      description = book_json.dig("volumeInfo", "description")
+
+      new_book = Book.new(
+        google_id: book_id,
+        title: title,
+        author: authors,
+        date: published_date,
+        description: description,
+        image_url: book_json.dig("volumeInfo", "imageLinks", "thumbnail")
+      )
+
+    BookshelfBook.create(bookshelf_id: new_bookshelf.id, book_id: new_book.id)
+    new_book.save
+    new_bookshelf.save
+    end
 end
 
 
-def get_books_by_genre(genre)
-  book_ids = []
-  url = "https://www.googleapis.com/books/v1/volumes?q=subject:#{genre}&maxResults=20"
+books_by_id("bookclub_collection", bookclub_collection)
 
-  begin
-    data = URI.open(url).read
-  rescue OpenURI::HTTPError => e
-    if e.message.include?('429')
-      sleep(10)  # Back off for 5 seconds before retrying
-      retry
-    else
-      # Handle other HTTP errors
-      puts "HTTP Error: #{e.message}"
-      return []  # Return an empty array if there's an error
-    end
-  end
 
-  book_json = JSON.parse(data)
-  return [] unless book_json["items"]
+# def get_books_by_genre(genre)
+#   book_ids = []
+#   url = "https://www.googleapis.com/books/v1/volumes?q=subject:#{genre}&maxResults=20"
 
-  books = []
-  book_json["items"].each do |book|
-    book_ids << book["id"]
-  end
+#   begin
+#     data = URI.open(url).read
+#   rescue OpenURI::HTTPError => e
+#     if e.message.include?('429')
+#       sleep(10)
+#       retry
+#     else
+#       # Handle other HTTP errors
+#       puts "HTTP Error: #{e.message}"
+#       return []
+#     end
+#   end
 
-  book_ids.each do |book_id|
-    begin
-      id_data = URI.open("https://www.googleapis.com/books/v1/volumes/#{book_id}").read
-    rescue OpenURI::HTTPError => e
-      if e.message.include?('429')
-        sleep(10)  # Back off for 5 seconds before retrying
-        retry
-      else
-        # Handle other HTTP errors
-        puts "HTTP Error: #{e.message}"
-        next  # Continue to the next book ID if there's an error
-      end
-    end
+#   book_json = JSON.parse(data)
+#   return [] unless book_json["items"]
 
-    id_json = JSON.parse(id_data)
-    title = id_json.dig("volumeInfo", "title")
-    authors = id_json.dig("volumeInfo", "authors")&.join(", ")
-    published_date = id_json.dig("volumeInfo", "publishedDate")
-    description = id_json.dig("volumeInfo", "description")
-    image_url = id_json.dig("volumeInfo", "imageLinks", "thumbnail")
+#   books = []
+#   book_json["items"].each do |book|
+#     book_ids << book["id"]
+#   end
 
-    Book.create(
-      google_id: book_id,
-      title: title,
-      author: authors,
-      date: published_date,
-      description: description,
-      image_url: image_url,
-      genre: genre
-    )
-  end
-end
+#   book_ids.each do |book_id|
+#     begin
+#       id_data = URI.open("https://www.googleapis.com/books/v1/volumes/#{book_id}").read
+#     rescue OpenURI::HTTPError => e
+#       if e.message.include?('429')
+#         sleep(10)
+#         retry
+#       else
+#         puts "HTTP Error: #{e.message}"
+#         next
+#       end
+#     end
 
-get_books_by_genre('Romance')
-get_books_by_genre('Crime')
-get_books_by_genre('Vampire')
-get_books_by_genre('Humor')
-get_books_by_genre('Horror')
-get_books_by_genre('Science Fiction')
-get_books_by_genre('Fantasy')
-get_books_by_genre('Mystery')
-get_books_by_genre('Thriller')
-get_books_by_genre('Historical Fiction')
+#     id_json = JSON.parse(id_data)
+#     title = id_json.dig("volumeInfo", "title")
+#     authors = id_json.dig("volumeInfo", "authors")&.join(", ")
+#     published_date = id_json.dig("volumeInfo", "publishedDate")
+#     description = id_json.dig("volumeInfo", "description")
+#     image_url = id_json.dig("volumeInfo", "imageLinks", "thumbnail")
+
+#     Book.create(
+#       google_id: book_id,
+#       title: title,
+#       author: authors,
+#       date: published_date,
+#       description: description,
+#       image_url: image_url,
+#       genre: genre
+#     )
+#   end
+# end
+
+# get_books_by_genre('Romance')
+# get_books_by_genre('Crime')
+# get_books_by_genre('Vampire')
+# get_books_by_genre('Humor')
+# get_books_by_genre('Horror')
+# get_books_by_genre('Science Fiction')
+# get_books_by_genre('Fantasy')
+# get_books_by_genre('Mystery')
+# get_books_by_genre('Thriller')
+# get_books_by_genre('Historical Fiction')
 
 # seeding some users and adding them to sessions
 10.times do |i|
