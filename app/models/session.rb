8d1f556be06f8530fc60a6cce9355d00cc9fb235
Tahmid Chowdhury <tahmid.chowdhury98@gmail.com>
2024-03-28 21:42:48 +0000

    Chatroom associated with session

--- app/models/session.rb
+++ app/models/session.rb
@@ -4,4 +4,13 @@ class Session < ApplicationRecord
   has_many :attendees
   has_many :users, through: :attendees
   has_many :agendas, dependent: :destroy
+  has_one :chatroom, dependent: :destroy
+
+  after_create :create_chatroom_for_session
+
+  private
+
+  def create_chatroom_for_session
+    create_chatroom(name: "Chat for #{book.title} Session #{id}")
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,17 @@
-require 'json'
-require 'open-uri'
-
 User.create!(
   email: "admin@admin.com",
   password: "asdfgh"
 )
 
 bookclub_collection = ["I12oPwAACAAJ", "u7XrDwAAQBAJ", "xDtkEAAAQBAJ", "49nUEAAAQBAJ", "hIx4EAAAQBAJ", "tkZFEAAAQBAJ", "Es_PCwAAQBAJ", "hxL2qWMAgv8C", "jVB1DwAAQBAJ", "nNjTDwAAQBAJ", "wxBbEAAAQBAJ", "cpAREAAAQBAJ", "OyCtDwAAQBAJ", "iBg_EAAAQBAJ", "lGjFtMRqp_YC"]
-
 romance_books = [  "IGkwDwAAQBAJ","1I1xDwAAQBAJ",  "JAzxznMKiq4C",  "Xb2GEAAAQBAJ",  "jwCVEAAAQBAJ",  "crZ1EAAAQBAJ",  "JXW7DwAAQBAJ",  "NlYD0AEACAAJ",  "Ra7tBgAAQBAJ",  "xT44AQAAQBAJ"]
-
 crime_books = ["hxL2qWMAgv8C", "7AiTEAAAQBAJ", "M8h6EAAAQBAJ", "bL6LEAAAQBAJ", "tpZcDwAAQBAJ", "nXrxGCVd9UoC", "z7SnEAAAQBAJ", "3rOaEAAAQBAJ", "gQisEAAAQBAJ", "a6NnDwAAQBAJ"]
-
 science_fiction_books = ["Nv-qDwAAQBAJ", "0OidDwAAQBAJ", "NljT2Bn3Y-wC", "52k6DwAAQBAJ", "TOMDq0vqXIQC", "IGvdhaVC5QwC", "ThS9hnCaNmkC", "uiVHDwAAQBAJ", "eCIFEAAAQBAJ", "iEiHEAAAQBAJ"]
-
-
-
 
 def books_by_id(bookshelf_name, initial_books)
 
   bookshelf = Bookshelf.new(name: bookshelf_name, user_id: 1)
   bookshelf.save
-
 
   initial_books.each do |book_id|
 
@@ -47,17 +37,50 @@ def books_by_id(bookshelf_name, initial_books)
     end
 end
 
-
 books_by_id("bookclub_collection", bookclub_collection)
 books_by_id("romance_books", romance_books)
 books_by_id("crime_books", crime_books)
 books_by_id("science_fiction_books", science_fiction_books)
 
+# seeding some users and adding them to sessions
+10.times do |i|
+  User.create!(
+    email: "user#{i}@example.com",
+    password: "password"
+  )
+end
 
+# seeding sessions
+books = Book.all
+users = User.all
 
+books.each do |book|
+  rand(1..5).times do
+    start_time = rand(1..14).days.from_now.beginning_of_day + rand(0..24).hours
+    book.sessions.create!(
+      book_id: book.id,
+      user_id: users.sample.id,
+      capacity: 15,
+      start_time: start_time,
+      video_link: 'http://example.com/session_dune',
+      end_time: 2.months.from_now
+    )
+  end
+end
 
-Chatroom.create!(name: "to make it work for now")
+# seeding attendees to sessions
+users = User.all
 
+Session.find_each do |session|
+  Attendee.create!(
+    session: session,
+    user: users.sample
+  )
+end
+
+#inactive code below:
+
+# Chatroom.create!(name: "to make it work for now")
 
 # def get_books_by_genre(genre)
 #   book_ids = []
@@ -127,35 +150,6 @@ Chatroom.create!(name: "to make it work for now")
 # get_books_by_genre('Thriller')
 # get_books_by_genre('Historical Fiction')
 
-# seeding some users and adding them to sessions
-10.times do |i|
-  User.create!(
-    email: "user#{i}@example.com",
-    password: "password"
-  )
-end
-
-# seeding sessions
-books = Book.all
-users = User.all
-
-books.each do |book|
-  rand(1..5).times do
-    start_time = rand(1..14).days.from_now.beginning_of_day + rand(0..24).hours
-    book.sessions.create!(
-      book_id: book.id,
-      user_id: users.sample.id,
-      capacity: 15,
-      start_time: start_time,
-      video_link: 'http://example.com/session_dune',
-      end_time: 2.months.from_now
-    )
-  end
-end
-
-
-
-
 # start_date = Date.today.beginning_of_week
 # end_date = 2.months.from_now
 
@@ -178,13 +172,3 @@ end
 #     video_link: 'http://example.com/session_twilight'
 #   )
 # end
-
-# seeding attendees to sessions
-users = User.all
-
-Session.find_each do |session|
-  Attendee.create!(
-    session: session,
-    user: users.sample
-  )
-end
