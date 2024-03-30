dedd1b112fbff197a78c893a30e037770748157a
Dominic Welds <dominicwelds@googlemail.com>
2024-03-30 13:26:19 +0000

    start meeting and chat both work

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 import { createConsumer } from "@rails/actioncable"
 
+// Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
   static values = { chatroomId: Number, currentUserId: Number }
   static targets = ["messages"]
@@ -8,37 +9,27 @@ export default class extends Controller {
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => this.handleReceivedData(data) }
+      { received: data => this.#handleData(data) }
     )
     console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
   }
 
-  handleReceivedData(data) {
-    switch(data.action) {
-      case 'execute_function':
-        this.handleExecuteFunction(data);
-        break;
-      case 'message':
-        this.handleChatMessage(data);
-        break;
-
+  #handleData(data) {
+    if (data.message) {
+      this.#insertMessageAndScrollDown(data)
     }
+    if (data.action === "execute_function") {
+      this.#executeFunction(data)
+    }
+
   }
 
-  handleExecuteFunction(data) {
+  #executeFunction(data) {
     switch(data.function_name) {
       case 'handleInitiateCall':
         this.handleInitiateCall();
         break;
-
     }
-  }
-
-  handleChatMessage(data) {
-    const currentUserIsSender = this.currentUserIdValue === data.sender_id
-    const messageElement = this.buildMessageElement(currentUserIsSender, data.message)
-    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
-    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
 
   handleInitiateCall() {
@@ -46,28 +37,30 @@ export default class extends Controller {
 
   }
 
-  executeFunction(functionName, functionArguments) {
-    // You can implement logic here to execute the specified function
-    // For example:
-    window[functionName](...functionArguments)
+
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
   }
 
-  buildMessageElement(currentUserIsSender, message) {
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
+  }
+
+  #buildMessageElement(currentUserIsSender, message) {
     return `
-      <div class="message-row d-flex ${this.justifyClass(currentUserIsSender)}">
-        <div class="${this.userStyleClass(currentUserIsSender)}">
+      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+        <div class="${this.#userStyleClass(currentUserIsSender)}">
           ${message}
         </div>
       </div>
     `
   }
 
-  justifyClass(currentUserIsSender) {
-    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
-  }
-
-  userStyleClass(currentUserIsSender) {
-    return currentUserIsSender ? "sender-style" : "receiver-style"
+  #insertMessageAndScrollDown(data) {
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
 
   resetForm(event) {
